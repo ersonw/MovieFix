d88134a51775668d58eb2aa1500f4296492bd130
ersonw <asoonnode@gmail.com>
2022-08-13 04:14:38 +0800

    2022080130402

--- lib/AssetsBackground.dart
+++ lib/AssetsBackground.dart
@@ -7,4 +7,7 @@ class AssetsBackground {
   static const String gold = ('assets/background/gold.png');
   static const String diamond = ('assets/background/diamond.png');
   static const String glory = ('assets/background/glory.png');
+  static const String membership1 = ('assets/background/membership1.png');
+  static const String membership2 = ('assets/background/membership2.png');
+  static const String membership3 = ('assets/background/membership3.png');
 }
--- lib/Page/MembershipPage.dart
+++ lib/Page/MembershipPage.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_fix/AssetsBackground.dart';
+import 'package:movie_fix/AssetsMembership.dart';
+import 'package:movie_fix/Global.dart';
 import 'package:movie_fix/Module/GeneralRefresh.dart';
+import 'package:movie_fix/Module/cAvatar.dart';
+import 'package:movie_fix/tools/Tools.dart';
 
 class MembershipPage extends StatefulWidget{
   @override
@@ -12,7 +18,248 @@ class _MembershipPage extends State<MembershipPage>{
   Widget build(BuildContext context) {
     return GeneralRefresh(
       title: '会员中心',
-
+      children: [
+        _buildHeader(),
+        _buildMemberShip(),
+        Container(
+          margin: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('会员尊享权益',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+  _buildMemberShip() {
+    List<Widget> widgets = [];
+    // for (int i = 0; i < records.length; i++) {
+    //   Video record = records[i];
+    //   widgets.add(InkWell(
+    //     onTap: () {},
+    //     child: Container(
+    //       margin: const EdgeInsets.all(6),
+    //       // color: Colors.red,
+    //       child: Column(
+    //         mainAxisSize: MainAxisSize.min,
+    //         children: [
+    //           Container(
+    //             height: 100,
+    //             width: 200,
+    //             decoration: BoxDecoration(
+    //               image: DecorationImage(
+    //                 image: NetworkImage(record.picThumb),
+    //                 fit: BoxFit.fill,
+    //               ),
+    //               borderRadius: BorderRadius.all(Radius.circular(9)),
+    //             ),
+    //           ),
+    //           Container(
+    //             width: 200,
+    //             child: Text(
+    //               record.title,
+    //               style: TextStyle(fontWeight: FontWeight.w300, fontSize: 13),
+    //               textAlign: TextAlign.left,
+    //               overflow: TextOverflow.fade,
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ));
+    // }
+    // if (widgets.length < 1) return Container();
+    return Container(
+      margin: const EdgeInsets.only(left: 15,top: 30),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Flex(
+          direction: Axis.horizontal,
+          // children: widgets,
+          children: [
+            Container(
+              margin: const EdgeInsets.all(9),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width - 36,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(AssetsBackground.bronze),
+                        fit: BoxFit.fill,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: Center(child: Text('青铜会员',style: TextStyle(color: Color(0xff7f3b1b),fontSize: 18),)),
+                  ),
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width - 36,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: Center(child: Icon(Icons.lock)),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(9),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width - 36,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(AssetsBackground.bronze),
+                        fit: BoxFit.fill,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: Center(child: Text('青铜会员',style: TextStyle(color: Color(0xff7f3b1b),fontSize: 18),)),
+                  ),
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width - 36,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.3),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: Center(child: Icon(Icons.lock)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  _buildHeader(){
+    return Container(
+      height: 270,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AssetsBackground.membership1),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left:30, right:30),
+            height: 120,
+            // color: Colors.black,
+            child: Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(60)),
+                    border: Border.all(width: 2,color: Colors.white),
+                    image: DecorationImage(
+                      image: buildHeaderPicture(self: true),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                Expanded(child: Container(
+                  height: 72,
+                  margin: const EdgeInsets.only(left: 9),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Expanded(child: Text(userModel.user.nickname,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18))),
+                      // Text('开通会员，获取会员权益',style: TextStyle(fontSize: 15,color: Colors.white.withOpacity(0.6)),),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(AssetsMembership.bronze1Icon),
+                          Row(
+                            children: [
+                              buildProgress(int.parse((100/1000*100).toStringAsFixed(0))),
+                              Container(
+                                margin: const EdgeInsets.only(left: 3),
+                                child: RichText(
+                                  text: TextSpan(
+                                      text: '100/',
+                                      style: TextStyle(color: Colors.white),
+                                      children: [
+                                        TextSpan(
+                                          text: '1000',
+                                          style: TextStyle(color: Colors.white.withOpacity(0.6)),
+                                        ),
+                                      ]
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  buildProgress(int progress){
+    List<Widget> basic = [];
+    for(int i=0;i< 20;i++){
+      basic.add(Container(
+        height: 9,
+        width: 150/20,
+        margin: const EdgeInsets.all(1/2),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white.withOpacity(0.3)),
+        ),
+      ));
+    }
+    if(progress < 0) progress = 0;
+    if(progress > 100) progress = 100;
+    return Container(
+      height: 9,
+      width: 170,
+      margin: const EdgeInsets.only(left: 6),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.all(Radius.circular(3)),
+      ),
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          Row(
+            children: basic,
+          ),
+          Container(
+            height: 9,
+            width: 168/ 100 * progress,
+            margin: const EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
